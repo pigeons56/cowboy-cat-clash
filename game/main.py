6a55b6e7f84f6bb6f969081f26ea0ec903429bd6
pigeons56 <lowpolypigeons@gmail.com>
2024-05-08 14:51:25 -0400

    Screen classes

--- game/main.py
+++ game/main.py
@@ -3,6 +3,108 @@ import sys
 
 SCREEN_SIZE = (800,400)
 
+class Screen():
+    def __init__(self, background_path,
+                 screen_size=SCREEN_SIZE):
+        self.background = pg.image.load(background_path)
+        self.selected = None
+        self.screen = pg.display.set_mode(screen_size)
+        self.running = False
+        self.clock = pg.time.Clock()
+
+    def check_quit(self, event):
+        if event.type == pg.QUIT:
+            sys.exit()
+
+    def check_events(self, event):
+        pass
+
+    def loop_functions(self):
+        pass
+
+    def load(self):
+        pass
+
+    def loop(self):
+        self.running = True
+        while self.running:
+            self.screen.blit(self.background,(0,0))
+            self.loop_functions()
+
+            for event in pg.event.get():
+                self.check_quit(event)
+                self.check_events(event)
+
+            pg.display.update()
+            self.clock.tick(60)
+
+class Controls_Screen(Screen):
+    def __init__(self):
+        super().__init__("../assets/backgrounds/controls_screen_background.png")
+    
+    def check_events(self, event):
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_ESCAPE:
+                self.running = False
+                start_screen.loop()
+
+class Credits_Screen(Screen):
+    def __init__(self):
+        super().__init__("../assets/backgrounds/credits_screen_background.png")
+    
+    def check_events(self, event):
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_ESCAPE:
+                self.running = False
+                start_screen.loop()
+
+class Select_Screen(Screen):
+    def __init__(self):
+        super().__init__("../assets/backgrounds/select_screen_background.png")
+    
+    def check_events(self, event):
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_ESCAPE:
+                self.running = False
+                start_screen.loop()
+
+class Start_Screen(Screen):
+    def __init__(self):
+        self.start_button = Button(100, 200, 50, 100, 
+                              "../assets/buttons/start_button_unclicked.png",
+                              "../assets/buttons/start_button_clicked.png")
+        self.controls_button = Button(100, 200, 100, 150, 
+                              "../assets/buttons/controls_button_unclicked.png",
+                              "../assets/buttons/controls_button_clicked.png")
+        self.credits_button = Button(100, 200, 150, 200, 
+                              "../assets/buttons/credits_button_unclicked.png",
+                              "../assets/buttons/credits_button_clicked.png")
+        
+        self.title = Sprite(100, 300, 200, 100, "../assets/text/title.png") 
+
+        super().__init__("../assets/backgrounds/start_screen_background.png")
+    
+    def check_events(self, event):
+        if event.type == pg.MOUSEBUTTONDOWN:
+            if self.start_button.is_clicked(event.pos[0],event.pos[1]):
+                self.running = False
+                select_screen.loop()
+            elif self.controls_button.is_clicked(event.pos[0],event.pos[1]):
+                self.running = False
+                controls_screen.loop()        
+            elif self.credits_button.is_clicked(event.pos[0],event.pos[1]):
+                self.running = False
+                credits_screen.loop()        
+
+    def load(self):
+        pass
+
+    def loop_functions(self):
+        self.screen.blit(self.title.image,(self.title.left_x,self.title.left_y))
+        self.screen.blit(self.start_button.image,(self.start_button.left_x, self.start_button.left_y))
+        self.screen.blit(self.controls_button.image,(self.controls_button.left_x, self.controls_button.left_y))
+        self.screen.blit(self.credits_button.image,(self.credits_button.left_x, self.credits_button.left_y))
+
 class Sprite():
     def __init__(self, left_x, right_x,
                  left_y, right_y, 
@@ -33,3 +135,10 @@ if __name__ == "__main__":
     #Initialize pygame
     pg.init()
     pg.font.init()
+
+    start_screen = Start_Screen()
+    controls_screen = Controls_Screen()
+    credits_screen = Credits_Screen()
+    select_screen = Select_Screen()
+
+    start_screen.loop()
