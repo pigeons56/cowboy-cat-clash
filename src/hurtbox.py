2b6a6b2c1ffb44774e3d9630f2ec525eaef952c9
pigeons56 <88516772+pigeons56@users.noreply.github.com>
2024-05-20 14:21:12 -0400

    setting up hit&hurtboxes

---
+++ src/hurtbox.py
@@ -0,0 +1,42 @@
+class Hurtbox():
+    def __init__(self, left_x, right_x, left_y, right_y):
+        self._left_x = left_x
+        self._right_x = right_x
+        self._left_y = left_y
+        self._right_y = right_y
+    
+    @property
+    def left_x(self):
+        return self._left_x
+    
+    @left_x.setter
+    def left_x(self, left_x):
+        self._left_x = left_x
+
+    @property
+    def right_x(self):
+        return self._right_x
+    
+    @right_x.setter
+    def right_x(self, right_x):
+        self._right_x = right_x
+
+    @property
+    def left_y(self):
+        return self._left_y
+    
+    @left_y.setter
+    def left_y(self, left_y):
+        self._left_y = left_y
+
+    @property
+    def right_y(self):
+        return self._right_y
+    
+    @right_y.setter
+    def right_y(self, right_y):
+        self._right_y = right_y
+
+class Hitbox(Hurtbox):
+    def __init__(self, left_x, right_x, left_y, right_y):
+        super().__init__(left_x, right_x, left_y, right_y)
